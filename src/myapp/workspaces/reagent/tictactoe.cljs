7657a34947dc722fa69d14efaaa2080f41bfd95d
Ky Ky <kyky@kyky.ky>
2019-04-01 01:21:16 +0300

    *

--- src/myapp/workspaces/reagent/tictactoe.cljs
+++ src/myapp/workspaces/reagent/tictactoe.cljs
@@ -1,69 +1,96 @@
 (ns myapp.workspaces.reagent.tictactoe
-  (:require [reagent.core :as r]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as r]
+            [cljs.core.async :refer [>! <! put! chan alts!]]))
 
 
 (defn new-board [n]
   (vec (repeat n (vec (repeat n :blank)))))
 
 (def initial-app-state
-  {:text "Tic Tac Toe"
-   :board (new-board 3)})
+  {:text  "Tic Tac Toe"
+   :board (new-board 3)
+   :next-turn :user})
 
 (defonce app-state
-  (r/atom initial-app-state))
+         (r/atom initial-app-state))
+
+(def user-chan (chan))
+(def computer-chan (chan))
+
+(defn run []
+  (go
+    (loop []
+      (let [[action c] (alts! [user-chan computer-chan])]
+       (prn action c)
+       (recur)))))
+
+(defn run2 []
+  (go
+    (loop []
+      (let [action (<! user-chan)]
+        (prn action))
+      (let [action (<! computer-chan)]
+        (prn action))
+      (recur))))
 
 (defn computer-move []
-  (swap! app-state assoc-in [:board 0 0 ] :cross))
+  (swap! app-state assoc-in [:board 0 0] :cross))
 
 (defn blank [i j]
   [:rect
    {:width  0.9
     :height 0.9
-    :fill "skyblue"
-    :x i
-    :y j}])
+    :fill   "skyblue"
+    :x      i
+    :y      j}])
 
 (defn circle [i j]
   [:circle
-   {:r 0.45
-    :cx (+ 0.45 i)
-    :cy (+ 0.45 j)
+   {:r    0.45
+    :cx   (+ 0.45 i)
+    :cy   (+ 0.45 j)
     :fill "coral"}])
 
+
 (defn cross [i j]
-  [:g {:stroke "green"
-       :stroke-width 0.15
+  [:g {:stroke         "green"
+       :stroke-width   0.15
        :stroke-linecap "round"
        :transform
-       (str "translate(" (+ 0.2 i) "," (+ 0.2 j) ") "
-            "scale(0.55)")}
+           (str "translate(" (+ 0.2 i) "," (+ 0.2 j) ") "
+                "scale(0.55)")}
    [:line {:x1 0 :y1 0 :x2 1 :y2 1}]
    [:line {:x1 0 :y1 1 :x2 1 :y2 0}]])
 
 (defn main []
-  (fn []
-    [:center
+  (let [button-text "New Game"
+        _ (run)]
+    (fn []
+     [:center
       [:h1 (:text @app-state)]
       (into
         [:svg
-          {:view-box "0 0 3 3" :width 300 :height 300}]
+         {:view-box "0 0 3 3" :width 300 :height 300}]
         (let [n (count (:board @app-state))]
           (for [i (range n)
                 j (range n)]
             (assoc-in
               (case (get-in @app-state [:board i j])
-               :blank (blank i j)
-               :circle (circle i j)
-               :cross (cross i j))
+                :blank (blank i j)
+                :circle (circle i j)
+                :cross (cross i j))
               [1 :on-click]
               (fn tail-click [e]
                 (prn "Clicked " i j)
                 (swap! app-state assoc-in [:board i j] :circle)
                 (computer-move)
+                (put! user-chan :action/user-move)
                 (prn @app-state))))))
       [:p
        [:button
         {:on-click
          (fn new-game-click [e]
-           (swap! app-state assoc :board (new-board 3)))}
-        "New Game"]]]))
+           (swap! app-state assoc :board (new-board 3))
+           (put! user-chan :action/new-game))}
+        button-text]]])))
