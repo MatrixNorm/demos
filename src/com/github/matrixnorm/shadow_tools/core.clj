dde37ea415fdb9c496d1c0a32e3f196959de8b42
Ky Ky <kyky@kyky.ky>
2019-07-15 01:35:20 +0300

    *

--- src/com/github/matrixnorm/shadow_tools/core.clj
+++ src/com/github/matrixnorm/shadow_tools/core.clj
@@ -7,14 +7,15 @@
 
 (defn index-html
   {:shadow.build/stage :configure}
-  [build-state source-rel-path]
-  (let [cache-abs-path (.getAbsolutePath (:cache-dir build-state))
-        project-root (subs cache-abs-path
-                           0
-                           (str/index-of cache-abs-path ".shadow-cljs"))
-        dest-path (str project-root "cljsdev/index.html")
-        source-path (str project-root source-rel-path)]
-    (println source-path)
-    (println dest-path)
-    (copy-file source-path dest-path)
-    build-state))
+  ([build-state] (index-html build-state
+                             "src/com/github/matrixnorm/shadow_tools/index.html"))
+  ([build-state source-rel-path]
+   (let [cache-abs-path (.getAbsolutePath (:cache-dir build-state))
+         project-root (subs cache-abs-path 0
+                            (str/index-of cache-abs-path ".shadow-cljs"))
+         dest-path (str project-root "cljsdev/index.html")
+         source-path (str project-root source-rel-path)]
+     (println source-path)
+     (println dest-path)
+     (copy-file source-path dest-path)
+     build-state)))
