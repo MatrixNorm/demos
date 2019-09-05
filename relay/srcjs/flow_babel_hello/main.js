a65d54f29217f06256e4e63694b0155114f48b70
Ky Ky <kyky@kyky.ky>
2019-09-05 14:47:30 +0300

    *

--- relay/srcjs/flow_babel_hello/main.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-function square(n: number): number {
-  return n * n;
-}
-
-const x = square(2);
-console.log(x)
--- relay/webpack.config.js
+++ relay/webpack.config.js
@@ -1,10 +1,14 @@
+/* global require */
+/* global module */
+/* global process */
+/* global __dirname */
+
 const path = require('path');
-const util = require('util')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-function PP(obj) {
-  console.log(util.inspect(obj, {showHidden: false, depth: null}))
-}
+// function PP(obj) {
+//   console.log(util.inspect(obj, {showHidden: false, depth: null}))
+// }
 
 const builds = {};
 
@@ -19,7 +23,7 @@ const baseBuild = ({ buildId }) => env => {
 
   return {
     entry: {
-        "main": `./srcjs/${buildId}/main.js`
+        "main": `./src/${buildId}/main.js`
     },
     output: {
         path: path.resolve(__dirname, outputPath),
@@ -33,141 +37,30 @@ const baseBuild = ({ buildId }) => env => {
     devtool: 'source-map',
     plugins: [
         new HtmlWebpackPlugin()
-    ]
+    ],
+    resolve: {
+      extensions: ['.mjs', '.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }        
+        }
+      ],
+    }
   };
 };
 
-// const basedBuild = buildId => overrides => env => {
-//   const outputPath = env.development? 
-//                         `jsdev/${buildId}`:
-//                         `docs/js/${buildId}`;
-//   const mode = env.development? "development" : "production";
-//   return Object.assign({
-//     entry: {
-//         "main": `./srcjs/${buildId}/main.js`
-//     },
-//     output: {
-//         path: path.resolve(__dirname, outputPath),
-//         //publicPath: "/",
-//         filename: "[name].js"
-//     },
-//     mode: mode,
-//     devServer: {
-//         contentBase: `jsdev/${buildId}`
-//     },
-//     devtool: 'source-map',
-//     plugins: [
-//         new HtmlWebpackPlugin()
-//     ]
-//   }, overrides);
-// };
+builds.relay_hello = 
+  baseBuild({ buildId: 'relay_hello' })
 
-// builds.hello = basedBuild('hello')({
-//     entry: {
-//       "main": "./srcjs/hello/main.js",
-//       "service-worker": "./srcjs/hello/service-worker.js"
-//     },
-//     plugins: [
-//       new HtmlWebpackPlugin({
-//         excludeChunks: ["service-worker"]
-//       })
-//     ]
-// });
+builds.relay_pagination_demo = 
+  baseBuild({ buildId: 'relay_pagination_demo' })
 
-// builds.graphql_hello = basedBuild('graphql_hello')({});
-
-const typescriptMixin = ({ buildId }) => ({
-  entry: {
-    "main": `./srcjs/${buildId}/main.ts`
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env', 
-              '@babel/typescript'
-            ]
-          }
-        }        
-      }
-    ],
-  }
-});
-
-builds.typescript_babel_hello = (() => {
-  const buildId = 'typescript_babel_hello';
-  return [
-    baseBuild({ buildId }),
-    typescriptMixin({ buildId })
-  ];
-})();
-
-const flowMixin = () => ({
-  resolve: {
-    extensions: ['.mjs', '.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env', 
-              '@babel/preset-flow'
-            ],
-            plugins: [
-              "@babel/proposal-class-properties",
-              "@babel/proposal-object-rest-spread",
-              "@babel/plugin-proposal-optional-chaining",
-              "@babel/plugin-proposal-nullish-coalescing-operator"
-            ]
-          }
-        }        
-      }
-    ],
-  }
-});
-
-builds.flow_babel_hello = [
-  baseBuild({ buildId: 'flow_babel_hello' }),
-  flowMixin()
-];
-
-const relayMixin = () => {
-  let x = flowMixin();
-
-  let plugins = x.module.rules[0].use.options.plugins;
-  plugins.unshift('relay');
-
-  let presets = x.module.rules[0].use.options.presets;
-  presets.unshift('@babel/preset-react');
-
-  x.module.rules[0].use.options.plugins = plugins;
-  x.module.rules[0].use.options.presets = presets;
-
-  return x;
-};
-
-builds.relay_hello = [
-  baseBuild({ buildId: 'relay_hello' }),
-  relayMixin()
-];
-
-builds.relay_pagination_demo = [
-  baseBuild({ buildId: 'relay_pagination_demo' }),
-  relayMixin()
-];
 
 module.exports = env => {
 
@@ -177,19 +70,9 @@ module.exports = env => {
     console.log(`No build with id ${buildId}\n env: ${env}`)
   };
 
-  const buildsChain = builds[buildId];
-  if ( !buildsChain ) {
+  const build = builds[buildId];
+  if ( !build ) {
     return onBuildNotFound(env);
   }
-  let finalConfig = {};
-  for (let build of buildsChain) {
-    if (typeof build === 'function') {
-      finalConfig = Object.assign(finalConfig, build(env))
-    } 
-    else {
-      finalConfig = Object.assign(finalConfig, build)
-    }
-  }
-  PP(finalConfig)
-  return finalConfig;
+  return build(env)
 }
