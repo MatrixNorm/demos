9db5e42e6ce7299edb130e90271f0973841d2cad
Ky Ky <kyky@kyky.ky>
2019-09-30 20:29:25 +0300

    graphiql seems to be done with random css hack

--- relay/src/graphiql_demo/App.js
+++ relay/src/graphiql_demo/App.js
@@ -1,12 +1,36 @@
 import React, { useRef } from "react";
-import { graphql, buildSchema } from "graphql";
+import { graphql } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
 import GraphiQL from "graphiql";
-import schemaSDL from "raw-loader!./schema.graphql";
+import typeDefs from "raw-loader!./schema.graphql";
+import citiesData from "./data";
 
-const schema = buildSchema(schemaSDL);
+const resolvers = {
+  Query: {
+    cities: (_, args) => {
+      const nodes = citiesData.slice(0, 4);
+      const edges = nodes.map(node => ({ node, cursor: node.id }));
+      const pageInfo = {
+        hasNextPage: true,
+        hasPreviousPage: false,
+        startCursor: nodes[0].id,
+        endCursor: nodes[nodes.length - 1].id
+      };
+      return { edges, pageInfo };
+    }
+  },
+  Node: {
+    __resolveType(node) {
+      return node;
+    }
+  }
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 async function fetcher(graphQLParams) {
-  const resp = await graphql(schema, graphQLParams, {}, undefined, {});
+  console.log(graphQLParams)
+  const resp = await graphql(schema, graphQLParams.query, {}, undefined, {});
   console.log(resp);
   return resp;
 }
