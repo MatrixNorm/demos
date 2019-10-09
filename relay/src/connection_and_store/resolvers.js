41bb7b9d4f4351c8db73be0dacc2f39607e6fcce
Ky Ky <kyky@kyky.ky>
2019-10-09 19:33:32 +0300

    saving...

--- relay/src/connection_and_store/resolvers.js
+++ relay/src/connection_and_store/resolvers.js
@@ -1,10 +1,35 @@
 import citiesData from "./data";
 
+const citiesByName = citiesData.sort((a, b) => a.name < b.name);
+
 export default {
   Query: {
-    randomCity: () => {
-      const index = Math.floor(citiesData.length * Math.random())
-      return citiesData[index]
+    allCities: (_, args) => {
+      const { after, first, before, last } = args;
+      if (first) {
+        const startIndex = after
+          ? citiesByName.findIndex(city => city.id === after)
+          : 0;
+        const endIndex = startIndex + first;
+        const nodes = citiesByName.slice(startIndex, endIndex);
+        const edges = nodes.map(node => ({
+          node,
+          cursor: node.id
+        }));
+        return {
+          edges,
+          pageInfo: {
+            hasNextPage: endIndex < citiesByName.length,
+            hasPreviousPage: startIndex > 0,
+            startCursor: nodes[0]?.id,
+            endCursor: nodes[nodes.length - 1]?.id
+          }
+        };
+      }
+      if (last && before) {
+        const endIndex = citiesByName.findIndex(city => city.id === before);
+      }
+      throw "cannot paginate";
     }
   },
   Node: {
