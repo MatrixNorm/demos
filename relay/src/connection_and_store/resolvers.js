9e0a272b2c74036493f27c5d4eccb2a06a7827cc
Ky Ky <kyky@kyky.ky>
2019-10-09 20:07:40 +0300

    saving...

--- relay/src/connection_and_store/resolvers.js
+++ relay/src/connection_and_store/resolvers.js
@@ -1,35 +1,12 @@
 import citiesData from "./data";
+import * as pagination from "../utils/pagination";
 
 const citiesByName = citiesData.sort((a, b) => a.name < b.name);
 
 export default {
   Query: {
     allCities: (_, args) => {
-      const { after, first, before, last } = args;
-      if (first) {
-        const startIndex = after
-          ? citiesByName.findIndex(city => city.id === after)
-          : 0;
-        const endIndex = startIndex + first;
-        const nodes = citiesByName.slice(startIndex, endIndex);
-        const edges = nodes.map(node => ({
-          node,
-          cursor: node.id
-        }));
-        return {
-          edges,
-          pageInfo: {
-            hasNextPage: endIndex < citiesByName.length,
-            hasPreviousPage: startIndex > 0,
-            startCursor: nodes[0]?.id,
-            endCursor: nodes[nodes.length - 1]?.id
-          }
-        };
-      }
-      if (last && before) {
-        const endIndex = citiesByName.findIndex(city => city.id === before);
-      }
-      throw "cannot paginate";
+      return pagination(citiesByName, args);
     }
   },
   Node: {
---
+++ relay/src/utils/pagination.js
@@ -0,0 +1,34 @@
+export function paginate(array, { after, first, before, last }) {
+  if (first) {
+    return _paginate_forwards(array, first, after);
+  }
+  if (last && before) {
+    return _paginate_backwards(array, last, before);
+  }
+  throw "cannot paginate";
+}
+
+function _paginate_forwards(array, first, after) {
+  const startIndex = after
+    ? array.findIndex(city => city.id === after)
+    : 0;
+  const endIndex = startIndex + first;
+  const nodes = array.slice(startIndex, endIndex);
+  const edges = nodes.map(node => ({
+    node,
+    cursor: node.id
+  }));
+  return {
+    edges,
+    pageInfo: {
+      hasNextPage: endIndex < array.length,
+      hasPreviousPage: startIndex > 0,
+      startCursor: nodes[0]?.id,
+      endCursor: nodes[nodes.length - 1]?.id
+    }
+  };
+}
+
+function _paginate_backwards(array, last, before) {
+  const endIndex = array.findIndex(city => city.id === before);
+}
