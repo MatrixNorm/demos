fe924491853189048dd2da8e9cdf42345fcc3e40
Ky Ky <kyky@kyky.ky>
2019-10-14 21:03:13 +0300

    *

--- relay/src/observable/main.js
+++ relay/src/observable/main.js
@@ -1,4 +1,4 @@
-import { fromDomEvent, map, filter } from "./myobservable";
+import { fromDomEvent, map } from "./myobservable";
 
 const clicks = fromDomEvent(document.getElementById("btn"), "click");
 const timestamps = map(clicks, evt => evt.timeStamp);
@@ -8,24 +8,70 @@ const zStream = map(timestamps, val => [val, Math.random()]);
 
 const cleanUp = {};
 
-cleanUp["z"] = zStream.subscribe({
-  next: val => console.log("zStream: ", val)
-});
+// cleanUp["z"] = zStream.subscribe({
+//   next: val => console.log("zStream: ", val)
+// });
 
-cleanUp["timestamps"] = timestamps.subscribe({
-  next: val => console.log("clickTimestamps: ", val)
-});
+// cleanUp["timestamps"] = timestamps.subscribe({
+//   next: val => console.log("clickTimestamps: ", val)
+// });
 
 cleanUp["y"] = yStream.subscribe({
   next: val => console.log("yStream: ", val)
 });
 
-cleanUp["x"] = xStream.subscribe({
-  next: val => console.log("xStream: ", val)
-});
+// cleanUp["x"] = xStream.subscribe({
+//   next: val => console.log("xStream: ", val)
+// });
 
-cleanUp["clicks"] = clicks.subscribe({
-  next: evt => console.log("clicks: ", evt)
-});
+// cleanUp["clicks"] = clicks.subscribe({
+//   next: evt => console.log("clicks: ", evt)
+// });
 
 window.cleanUp = cleanUp;
+
+/*
+
+// yStream = map(xStream, val => [val, val % 7]);
+
+yStreamNext = val => console.log("yStream: ", val)
+
+yStream.subscribe({
+  next: yStreamNext
+});
+
+xStream.subscribe({
+  next: value => {
+    yStreamNext(fn(value));
+  }
+})
+
+xStreamNext = value => {
+  yStreamNext(fn(value));
+}
+
+// const xStream = map(timestamps, val => Math.floor(val));
+
+timestamps.subscribe({
+  next: value => {
+    xStreamNext(fn(value));
+  }
+})
+
+timestampsNext = value => {
+  xStreamNext(fn(value));
+}
+
+// const timestamps = map(clicks, evt => evt.timeStamp);
+
+clicks.subscribe({
+  next: value => {
+    timestampsNext(fn(value));
+  }
+})
+
+// clicks = fromDomEvent(document.getElementById("btn"), "click");
+
+!!! click
+
+*/
