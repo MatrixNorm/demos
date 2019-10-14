3f417899c0629cd542185fab7019e33abf1759ff
Ky Ky <kyky@kyky.ky>
2019-10-14 14:31:41 +0300

    saving

--- relay/src/observable/main.js
+++ relay/src/observable/main.js
@@ -1,91 +1,64 @@
-function createObservable(producer) {
-  const _subbers = {};
-  let _idCount = 0;
-  let _unsubFromProducer = function() {};
-
-  const next = value => {
-    Object.values(_subbers).every(subber => subber.next(value));
-  };
-
-  const error = err => {
-    Object.values(_subbers).every(subber => subber.error(err));
-  };
-
-  const complete = () => {
-    Object.values(_subbers).every(subber => subber.complete());
-  };
-
-  const _checkIfNoSubbersLeft = () => {
-    if (Object.keys(_subbers).length === 0) {
-      _unsubFromProducer();
-    }
-  };
-
+function createObservable(howToSubscribeToSomeProducer) {
   return {
     subscribe: subber => {
-      if (Object.keys(_subbers).length === 0) {
-        _unsubFromProducer = producer.subscribe({ next, error, complete });
-      }
-      _subbers[_idCount] = subber;
-      _idCount++;
-      return () => {
-        delete _subbers[_idCount];
-        _checkIfNoSubbersLeft();
-      };
+      howToSubscribeToSomeProducer(subber);
     }
   };
 }
 
 function map(observable, fn) {
-  createObservable();
-}
-
-function create(source) {
-  let _idCount = 0;
-  const _observers = {};
-  let _shutSourceDown = function() {};
-  let _isVirgin = true;
-  let _isClosed = false;
-
-  const _start_source = () => {
-    _shutSourceDown = source.subscribe({
+  createObservable(subber => {
+    observable.subscribe({
       next: value => {
-        Object.values(_observers).every(ob => ob.next(value));
-      },
-      error: err => {
-        Object.values(_observers).every(ob => ob.error(err));
-      },
-      complete: () => {
-        Object.values(_observers).every(ob => ob.complete());
+        subber.next(fn(value));
       }
     });
-    _isVirgin = false;
-  };
+  });
+}
 
-  const _stop_source = () => {
-    _shutSourceDown();
-    _isClosed = true;
-  };
-
-  return {
-    subscribe: (observer: Observer) => {
-      if (_isClosed) {
-        return;
-      }
-      _observers[_idCount] = observer;
-      _idCount++;
-      if (_isVirgin) {
-        _start_source();
-      }
-      return () => {
-        delete _observers[_idCount];
-        if (Object.keys(_observers).length === 0) {
-          _stop_source();
+function filter(observable, pred) {
+  createObservable(subber => {
+    observable.subscribe({
+      next: value => {
+        if (pred(value)) {
+          subber.next(value);
         }
-      };
-    },
-    isClosed: () => {
-      return _isClosed;
+      }
+    });
+  });
+}
+
+function fromDomEvent(domEl, eventName) {
+  let _subbers = {};
+  let _id = 0;
+
+  const onEvent = event => {
+    Object.values(_subbers).every(subber => subber.next(event));
+  };
+
+  const start = () => {
+    domEl.addEventListener(eventName, onEvent);
+  };
+
+  const cleanUp = () => {
+    domEl.removeEventListener(eventName, onEvent);
+  };
+
+  const checkForCleanUp = () => {
+    if (Object.keys(_subbers).length === 0) {
+      cleanUp();
     }
   };
+
+  return createObservable(subber => {
+    if (Object.keys(_subbers).length === 0) {
+      start();
+    }
+    let id = _id++;
+    _subbers[id] = subber;
+    return () => {
+      delete _subbers[id];
+      checkForCleanUp();
+    };
+  });
 }
