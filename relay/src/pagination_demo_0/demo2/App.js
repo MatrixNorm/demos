c23e905deb02f09ec0ab92cafbc62032ff6aa99d
Ky Ky <kyky@kyky.ky>
2019-11-03 22:55:00 +0300

    *

--- relay/src/pagination_demo_0/demo2/App.js
+++ relay/src/pagination_demo_0/demo2/App.js
@@ -4,95 +4,13 @@ import { QueryRenderer, graphql } from "react-relay";
 import React from "react";
 
 import environment from "./Environment";
-import PostFeed from "./PostFeed";
-import PostPagination from "./PostPagination";
-import PostPaginationControls from "./PostPaginationControls";
-
-import type { AppQueryResponse } from "./__generated__/AppQuery.graphql";
-
-const AppQuery = graphql`
-  query AppQuery(
-    $first: Int
-    $after: String
-    $last: Int
-    $before: String
-    $orderBy: PostOrderingInput
-  ) {
-    search1: search {
-      ...PostFeed_search
-        @arguments(
-          first: $first
-          after: $after
-          last: $last
-          before: $before
-          orderBy: $orderBy
-        )
-    }
-    sorting1: sorting {
-      ...PostFeed_sorting @arguments(postListingId: $postListingId_1)
-    }
-    search2: search {
-      ...PostFeed_search
-        @arguments(
-          first: $first
-          after: $after
-          last: $last
-          before: $before
-          orderBy: $orderBy
-        )
-    }
-    sorting2: sorting {
-      ...PostFeed_sorting @arguments(postListingId: $postListingId_2)
-    }
-  }
-`;
-
-type Props = {
-  +error: ?Error,
-  +props: ?AppQueryResponse
-};
-
-const render = ({ error, props }: Props) => {
-  if (error) {
-    return <div style={{ color: "red" }}>Error: {error.message}</div>;
-  }
-  if (props) {
-    return (
-      <>
-        {props.search1 && (
-          <PostFeed search={props.search1}>
-            <PostPaginationControls />
-            <PostPagination />
-          </PostFeed>
-        )}
-        {props.search2 && (
-          <PostFeed search={props.search2}>
-            <PostPagination />
-            <br />
-            <PostPaginationControls />
-          </PostFeed>
-        )}
-      </>
-    );
-  }
-  return <div>Loading...</div>;
-};
 
 const App = () => {
-  const initialVariables = {
-    first: 3,
-    after: null,
-    orderBy: { field: "createdAt", desc: true },
-    postListingId_1: "client:post-listing-1",
-    postListingId_2: "client:post-listing-2"
-  };
   return (
-    <QueryRenderer
-      query={AppQuery}
-      environment={environment}
-      variables={{ ...initialVariables }}
-      render={render}
-    />
+    <div>
+      <PostsView listingId="xxx1" />
+      <PostsView listingId="xxx2" />
+    </div>
   );
 };
 
--- relay/src/pagination_demo_0/demo2/PostsView.js
+++ relay/src/pagination_demo_0/demo2/PostsView.js
@@ -1,15 +1,45 @@
-import { createFragmentContainer, graphql } from "react-relay";
+// @flow
 
-function PostView({ orderingSettings }) {
-  const { activeField, allOrderings } = orderingSettings;
-  
+import React from "react";
+//$FlowFixMe
+import { LocalQueryRenderer, graphql } from "react-relay";
+import environment from "./env";
+
+function PostsView({ listingId }) {
+  return (
+    <LocalQueryRenderer
+      query={graphql`
+        query PostsViewQuery($listingId: ID!) {
+          __typename
+          localState {
+            postListingState(id: $listingId) {
+              ...OrderSelector_XXX
+              ...PostsListing_XXX
+            }
+          }
+        }
+      `}
+      environment={environment}
+      variables={{}}
+      render={({ error, props }) => {
+        if (error) throw error;
+        if (!props) return <h3>loading...</h3>;
+        return (
+          <div>
+            <OrderSelector localSettings={props.localSettings} />
+            <PostsListing localSettings={props.localSettings} />
+          </div>
+        );
+      }}
+    />
+  );
 }
 
-export default createFragmentContainer(PostView, {
-  orderingSettings: graphql`
-    fragment CitiesView_orderingSettings on PostListingState {
-      activeField
-      allOrderings
-    }
-  `
-});
+// export default createFragmentContainer(PostsView, {
+//   orderingSettings: graphql`
+//     fragment CitiesView_orderingSettings on PostListingState {
+//       activeField
+//       allOrderings
+//     }
+//   `
+// });
