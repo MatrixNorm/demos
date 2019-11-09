b67826cd589d46bbef5dadadda1ca3632524dd02
Ky Ky <kyky@kyky.ky>
2019-11-10 00:01:54 +0300

    *

--- relay/src/pagination_demo_0/demo2/clientResolvers.js
+++ relay/src/pagination_demo_0/demo2/clientResolvers.js
@@ -2,25 +2,7 @@ const localState = {
   postListing: {
     "1": {
       id: "1",
-      sorting: ["createdAt", "viewsCount"],
-      orders: {
-        createdAt: {
-          field: "createdAt",
-          desc: false,
-          fieldDescription_ASC: "AAA",
-          fieldDescription_DESC: "BBB",
-          isActive: true
-        },
-        viewsCount: {
-          field: "viewsCount",
-          desc: false,
-          fieldDescription_ASC: "XXX",
-          fieldDescription_DESC: "YYY"
-        }
-      }
-    },
-    "2": {
-      id: "2",
+      activeField: "createdAt",
       sorting: ["createdAt", "viewsCount"],
       orders: {
         createdAt: {
@@ -33,13 +15,39 @@ const localState = {
           field: "viewsCount",
           desc: false,
           fieldDescription_ASC: "XXX",
-          fieldDescription_DESC: "YYY",
-          isActive: true
+          fieldDescription_DESC: "YYY"
+        }
+      }
+    },
+    "2": {
+      id: "2",
+      activeField: "viewsCount",
+      sorting: ["createdAt", "viewsCount"],
+      orders: {
+        createdAt: {
+          field: "createdAt",
+          desc: false,
+          fieldDescription_ASC: "AAA",
+          fieldDescription_DESC: "BBB"
+        },
+        viewsCount: {
+          field: "viewsCount",
+          desc: false,
+          fieldDescription_ASC: "XXX",
+          fieldDescription_DESC: "YYY"
         }
       }
     }
   }
 };
+
+function postListingDbToGql(dbObj) {
+  return {
+    id: dbObj.id,
+    activeField: dbObj.activeField,
+    configuration: dbObj.sorting(field => dbObj.orders[field])
+  };
+}
 
 export const clientResolvers = {
   Query: {
@@ -52,7 +60,7 @@ export const clientResolvers = {
     postListing: ({ id }) => {
       console.log("postListing");
       const x = localState.postListing[id];
-      return x.sorting(field => x.orders[field]);
+      return postListingDbToGql(x);
     }
   },
   Node: {
@@ -63,15 +71,15 @@ export const clientResolvers = {
   Mutation: {
     changePostListingActiveField: (_, { input }) => {
       console.log(input);
-      localState.postListing[input.postListingId].activeField =
-        input.activeField;
-      return { ...input };
+      const x = localState.postListing[input.postListingId];
+      x.activeField = input.activeField;
+      return postListingDbToGql(x);
     },
     togglePostListingDirection: (_, { input }) => {
       console.log(input);
-      const activeField =
-        localState.postListing[input.postListingId].activeField;
-      return { ...input };
+      const x = localState.postListing[input.postListingId];
+      x.orders[x.activeField].desc = !x.orders[x.activeField].desc;
+      return postListingDbToGql(x);
     }
   }
 };
