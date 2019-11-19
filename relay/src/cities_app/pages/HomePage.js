74e81abd72016a7fa66c3c375b0b7d7224ddaf06
Ky Ky <kyky@kyky.ky>
2019-11-19 21:53:11 +0300

    *

--- relay/src/cities_app/pages/HomePage.js
+++ relay/src/cities_app/pages/HomePage.js
@@ -2,36 +2,27 @@
 
 import { QueryRenderer, graphql } from "react-relay";
 import React from "react";
-import ContinentSelector from "../components/ContinentSelector";
-import CitiesListView from "../components/CitiesListView";
-import clientEnv from "../clientEnv";
+import CitiesPagination from "../components/CitiesPagination";
+import environment from "../env";
 
-function CitiesListPage() {
+export default function HomePage() {
   return (
     <QueryRenderer
       query={graphql`
-        query CitiesListPageQuery {
-          localSettings {
-            id
-            ...ContinentSelector_localSettings
-            ...CitiesListView_localSettings
+        query HomePageQuery($pageNo: Int!) {
+          viewer {
+            ...CitiesPagination_cities
+              @arguments(continent: $continent, pageNo: $pageNo)
           }
         }
       `}
-      environment={clientEnv}
-      variables={{}}
+      environment={environment}
+      variables={{ pageNo: 0 }}
       render={({ error, props }) => {
         if (error) throw error;
         if (!props) return <h3>loading...</h3>;
-        return (
-          <div>
-            <ContinentSelector localSettings={props.localSettings} />
-            <CitiesListView localSettings={props.localSettings} />
-          </div>
-        );
+        return <CitiesPagination cities={props.viewer} />;
       }}
     />
   );
 }
-
-export default CitiesListPage;
