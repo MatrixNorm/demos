ee0e1534841a5753da6c0bc52b04403044d090d8
Ky Ky <kyky@kyky.ky>
2019-11-27 22:22:22 +0300

    *

--- relay/src/ui/suggestion_list/App.js
+++ relay/src/ui/suggestion_list/App.js
@@ -1,17 +1,33 @@
-// @flow
-
-import React, { useCallback, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useReducer,
+  useRef,
+  useState
+} from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
+const ReduxContext = React.createContext();
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "USER_SELECTED_SUGGESTION":
+      return { ...state, inputValue: action.suggestion };
+    default:
+      return state;
+  }
+}
+
 export default function App() {
   const inputEl = useRef(null);
-
+  const [state, dispatch] = useReducer(reducer, { inputValue: "" });
+  console.log("render", state);
   return (
-    <>
+    <ReduxContext.Provider value={{ state, dispatch }}>
       <input type="text" ref={inputEl} />
       <SuggestionList />
-    </>
+    </ReduxContext.Provider>
   );
 }
 
@@ -19,9 +35,9 @@ function SuggestionList() {
   const [items] = useState(["Aa", "Bb", "Cc", "Dd", "Ee"]);
   const [selectedIdx, setSelectedIdx] = useState(null);
 
-  const onSelectListItem = useCallback(index => {
-    setSelectedIdx(index);
-  }, []);
+  // const onSelectListItem = useCallback(index => {
+  //   setSelectedIdx(index);
+  // }, []);
   console.log("render", selectedIdx);
   return (
     <div>
@@ -32,24 +48,16 @@ function SuggestionList() {
         `}
       >
         {items.map((item, j) => (
-          <SuggestionListItem
-            key={j}
-            index={j}
-            onSelectListItem={onSelectListItem}
-            text={item}
-          />
+          <SuggestionListItem key={j} index={j} text={item} />
         ))}
       </ul>
     </div>
   );
 }
 
-const SuggestionListItem = React.memo(function({
-  index,
-  text,
-  onSelectListItem
-}) {
+const SuggestionListItem = React.memo(function({ index, text }) {
   const [isHovered, setIsHovered] = useState(false);
+  const { dispatch } = useContext(ReduxContext);
   console.log(text, isHovered);
   const style = isHovered
     ? css`
@@ -61,7 +69,12 @@ const SuggestionListItem = React.memo(function({
       css={style}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      onClick={() => onSelectListItem(index)}
+      onClick={() =>
+        dispatch({
+          type: "USER_SELECTED_SUGGESTION",
+          suggestion: text
+        })
+      }
     >
       {text}
     </li>
