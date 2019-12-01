d2685131543bf93d9f65268a3c1a97ac6053f132
Ky Ky <kyky@kyky.ky>
2019-12-02 00:33:25 +0300

    *

--- relay/src/react/setstate_and_props/main.js
+++ relay/src/react/setstate_and_props/main.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
+function App() {
+  console.log("App");
+  const [inputInitialValue, setInputInitialValue] = useState("");
+  return (
+    <div>
+      <button
+        onClick={() => setInputInitialValue(new Date().getMilliseconds())}
+      >
+        Reset input
+      </button>
+      <TextInput initialValue={inputInitialValue} />
+    </div>
+  );
+}
+
+function TextInput({ initialValue }) {
+  const [value, setValue] = useState(initialValue);
+  console.log("TextInput", initialValue, value);
+
+  useEffect(() => {
+    console.log("useEffect");
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return (
+    <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
