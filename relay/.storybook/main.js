92d72defee92da9017bac69e509cc7c54fe5f0eb
Ky Ky <kyky@kyky.ky>
2020-01-28 22:41:03 +0300

    *

--- relay/.storybook/main.js
+++ relay/.storybook/main.js
@@ -3,15 +3,28 @@ module.exports = {
   addons: ["@storybook/addon-actions", "@storybook/addon-links"],
   webpackFinal: config => {
     let plugins = config.module.rules[0].use[0].options.plugins;
-    let presets = config.module.rules[0].use[0].options.presets;
-    plugins.push("@babel/plugin-proposal-optional-chaining");
-    plugins.push([
-      "relay",
-      {
-        artifactDirectory: `./src/__relay__`
-      }
-    ]);
-    presets.push("@babel/typescript");
+    config.module.rules[0].use[0].options.plugins = [
+      [
+        "relay",
+        {
+          artifactDirectory: `./src/__relay__`
+        }
+      ],
+      "@babel/plugin-proposal-optional-chaining",
+      ...plugins
+    ];
+
+    config.module.rules[0].use[0].options.presets = [
+      "@babel/preset-react",
+      ["@babel/preset-env", { targets: { chrome: "76" } }],
+      "@babel/typescript"
+    ];
+
+    config.module.rules[0].test = /\.(ts|tsx|js|jsx|mjs)$/;
+
+    config.resolve.extensions = [".mjs", ".js", ".jsx", ".ts", ".tsx"];
+    //config.resolve.alias['']
+
     console.dir(config, { depth: null });
     return config;
   }
