6aee1f1b614e548124be79fb465b9bb0556e8c78
Ky Ky <kyky@kyky.ky>
2020-02-01 00:48:19 +0300

    *

--- relay/src/cities_app/demo1/stories/SearchParametersStory.stories.tsx
+++ relay/src/cities_app/demo1/stories/SearchParametersStory.stories.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import { QueryRenderer, graphql } from "react-relay";
+import { createTestingEnv } from "../env";
 import { SearchParametersPresentational } from "../components/SearchParametersPresentational";
+import SearchParameters from "../components/SearchParameters";
 
-export default { title: "cities_app-demo1/SearchParams" };
+export default { title: "cities_app-demo1/SearchParameters" };
 
 export const aaa1 = () => {
   const fieldValues = {
@@ -18,6 +21,46 @@ export const aaa1 = () => {
       fieldValues={fieldValues}
       onFieldChange={onFieldChange}
       onButtonClick={() => {}}
+    />
+  );
+};
+
+export const bbb2 = () => {
+  const environment = createTestingEnv({
+    Query: {
+      citiesMetadata: () => {
+        return {
+          populationLowerBound: 2,
+          populationUpperBound: 8
+        };
+      }
+    }
+  });
+  return (
+    <QueryRenderer
+      query={graphql`
+        query SearchParametersStoryQuery {
+          citiesMetadata {
+            ...SearchParameters_metadata
+          }
+        }
+      `}
+      environment={environment}
+      variables={{}}
+      render={({ error, props }) => {
+        return (
+          <SearchParameters
+            metadata={props.citiesMetadata}
+            initialSearchParams={{
+              countryNameContains: "q",
+              populationGte: null,
+              populationLte: null
+            }}
+            environment={environment}
+            refetch={() => {}}
+          />
+        );
+      }}
     />
   );
 };
