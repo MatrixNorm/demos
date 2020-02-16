68ef2197098d6debe2e5c7a06548ca657584136f
Ky Ky <kyky@kyky.ky>
2020-02-16 20:15:00 +0300

    *

--- relay/src/cities_app/components/UserSettings.tsx
+++ relay/src/cities_app/components/UserSettings.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+import { useState } from "react";
 import { graphql, createFragmentContainer } from "react-relay";
 import styled from "styled-components";
 import UpdateUserSettingsMutation from "../mutations/UpdateUserSettingsMutation";
-import { NumberInput } from "../elements/Inputs";
+import { NumberInput, TextInput } from "../elements/Inputs";
+import { SubmitButton } from "../elements/Buttons";
 import { UserSettings_user } from "__relay__/UserSettings_user.graphql";
 
 type Props = {
@@ -14,14 +16,33 @@ export const UserSettings = styled.section``;
 
 export default createFragmentContainer(
   ({ user, relay }: Props) => {
-    const handlePaginationPageSizeChange = (
+    const [locCache, setLocCache] = useState(user.settings);
+    const handleCitiesPaginationPageSize = (
       e: React.ChangeEvent<HTMLInputElement>
     ) => {
+      setLocCache({
+        ...locCache,
+        citiesPaginationPageSize: Number(e.target.value)
+      });
+    };
+    const handleFoo = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setLocCache({
+        ...locCache,
+        foo: e.target.value
+      });
+    };
+    const handleBar = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setLocCache({
+        ...locCache,
+        bar: Number(e.target.value)
+      });
+    };
+    const handleSubmit = () => {
       UpdateUserSettingsMutation.commit({
         environment: relay.environment,
         input: {
           userId: user.id,
-          citiesPaginationPageSize: Number(e.target.value)
+          ...locCache
         }
       });
     };
@@ -30,9 +51,14 @@ export default createFragmentContainer(
         <span>Pagination Page Size: </span>
         <NumberInput
           step="1"
-          value={user.settings?.citiesPaginationPageSize || ""}
-          onChange={handlePaginationPageSizeChange}
+          value={user.settings.citiesPaginationPageSize}
+          onChange={handleCitiesPaginationPageSize}
         />
+        <span>Foo: </span>
+        <TextInput value={user.settings.foo} onChange={handleFoo} />
+        <span>Bar: </span>
+        <NumberInput step="1" value={user.settings.bar} onChange={handleBar} />
+        <SubmitButton onChange={handleSubmit}>Save</SubmitButton>
       </UserSettings>
     );
   },
