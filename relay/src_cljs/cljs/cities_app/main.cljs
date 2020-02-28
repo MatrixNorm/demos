ebe9fa28ccedd83b38d4f395e822a68ef2efac8d
Ky Ky <kyky@kyky.ky>
2020-02-29 01:00:07 +0300

    *

--- relay/src_cljs/cljs/cities_app/main.cljs
+++ relay/src_cljs/cljs/cities_app/main.cljs
@@ -1,8 +1,12 @@
 (ns cljs.cities-app.main
   (:require
-   ["/cities_app/resolvers/citiesPagination" :as page]
+   ["/cities_app/resolvers/citiesPagination" :as citiesPagination]
    [clojure.test.check :as tc]
    [clojure.test.check.generators :as gen]
    [clojure.test.check.properties :as prop :include-macros true]))
 
-page
+citiesPagination
+
+(def cityGen gen/int)
+
+(gen/sample (gen/map gen/keyword gen/int))
