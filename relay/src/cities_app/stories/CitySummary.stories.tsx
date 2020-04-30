ec12d4b60b59df06e627a5cbc9cbb320cb8fa97a
Ky Ky <kyky@kyky.ky>
2020-04-30 21:23:55 +0300

    *

--- relay/src/cities_app/stories/CitySummary.stories.tsx
+++ relay/src/cities_app/stories/CitySummary.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { QueryRenderer, graphql } from "react-relay";
+import { graphql, QueryRenderer, LocalQueryRenderer } from "react-relay";
 import {
   createOperationDescriptor,
   getRequest,
@@ -15,6 +15,14 @@ import { CitySummaryStoryQuery } from "__relay__/CitySummaryStoryQuery.graphql";
 
 export default { title: "cities_app-demo1/CitySummary" };
 
+const query = graphql`
+  query CitySummaryStoryQuery($cityId: ID!) {
+    city: node(id: $cityId) {
+      ...CitySummary_city
+    }
+  }
+`;
+
 export const citySummary = () => {
   const environment = returnPayloadEnvironment({
     city: {
@@ -27,17 +35,10 @@ export const citySummary = () => {
   });
   return (
     <QueryRenderer<CitySummaryStoryQuery>
-      query={graphql`
-        query CitySummaryStoryQuery($cityId: ID!) {
-          city: node(id: $cityId) {
-            ...CitySummary_city
-          }
-        }
-      `}
+      query={query}
       environment={environment}
-      variables={{ cityId: "" }}
+      variables={{ cityId: "1" }}
       render={({ props }) => {
-        console.log(props.city);
         return props && props.city && <CitySummary city={props.city} />;
       }}
     />
@@ -46,15 +47,8 @@ export const citySummary = () => {
 
 export const citySummarySkeleton = () => {
   const env = returnPayloadEnvironment(defaultData);
-  const query = graphql`
-    query CitySummaryStory2Query($cityId: ID!) {
-      city: node(id: $cityId) {
-        ...CitySummary_city
-      }
-    }
-  `;
   const request = getRequest(query);
-  const operation = createOperationDescriptor(request, { cityId: "" });
+  const operation = createOperationDescriptor(request, { cityId: "1" });
   let data = {
     city: {
       __typename: "City",
@@ -68,19 +62,123 @@ export const citySummarySkeleton = () => {
   console.log(env.getStore().getSource());
   let response = env.lookup(operation.fragment);
   console.log(response);
-  return null;
-  // return (
-  //   <QueryRenderer<CitySummaryStoryQuery>
-  //     query={query}
-  //     environment={loadingForeverEnvironment()}
-  //     variables={{ cityId: "" }}
-  //     render={({ props }) => {
-  //       console.log(response.data.city);
-  //       if (props === null) {
-  //         return <CitySummary city={response.data.city} />;
-  //       }
-  //       return null;
-  //     }}
-  //   />
-  // );
+  return (
+    <QueryRenderer<CitySummaryStoryQuery>
+      query={query}
+      environment={loadingForeverEnvironment()}
+      variables={{ cityId: "1" }}
+      render={({ props }) => {
+        console.log(response.data.city);
+        if (props === null) {
+          return <CitySummary city={response.data.city} />;
+        }
+        return null;
+      }}
+    />
+  );
+};
+
+export const citySummarySkeleton2 = () => {
+  return (
+    <QueryRenderer<CitySummaryStoryQuery>
+      query={query}
+      environment={loadingForeverEnvironment()}
+      variables={{ cityId: "1" }}
+      render={({ props }) => {
+        if (props === null) {
+          console.log(222222222222);
+          return (
+            <QueryRenderer<CitySummaryStoryQuery>
+              query={query}
+              environment={returnPayloadEnvironment({
+                city: {
+                  __typename: "City",
+                  id: "1",
+                  name: "Madrid",
+                  country: "Spain",
+                  population: 3600000,
+                },
+              })}
+              variables={{ cityId: "1" }}
+              render={({ props }) => {
+                //return <div>xxxx</div>;
+                return props && props.city && <CitySummary city={props.city} />;
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
+};
+
+export const citySummarySkeleton3 = () => {
+  const env = returnPayloadEnvironment(defaultData);
+  const request = getRequest(query);
+  const operation = createOperationDescriptor(request, { cityId: "1" });
+  let data = {
+    city: {
+      __typename: "City",
+      id: "1",
+      name: "aaaaaa",
+      country: "bbbbbbbb",
+      population: 1000000,
+    },
+  };
+  env.commitPayload(operation, data);
+  console.log(env.getStore().getSource()._records);
+  let response = env.lookup(operation.fragment);
+  console.log(response);
+  return (
+    <LocalQueryRenderer<CitySummaryStoryQuery>
+      query={query}
+      environment={env}
+      variables={{ cityId: "1" }}
+      render={({ props }) => {
+        return props && props.city && <CitySummary city={props.city} />;
+      }}
+    />
+  );
+};
+
+export const citySummarySkeleton4 = () => {
+  return (
+    <QueryRenderer<CitySummaryStoryQuery>
+      query={query}
+      environment={loadingForeverEnvironment()}
+      variables={{ cityId: "1" }}
+      render={({ props }) => {
+        if (props === null) {
+          const env = returnPayloadEnvironment(defaultData);
+          const request = getRequest(query);
+          const operation = createOperationDescriptor(request, { cityId: "1" });
+          let data = {
+            city: {
+              __typename: "City",
+              id: "1",
+              name: "aaaaaa",
+              country: "bbbbbbbb",
+              population: 1000000,
+            },
+          };
+          env.commitPayload(operation, data);
+          console.log(env.getStore().getSource()._records);
+          let response = env.lookup(operation.fragment);
+          console.log(response);
+          return (
+            <LocalQueryRenderer<CitySummaryStoryQuery>
+              query={query}
+              environment={env}
+              variables={{ cityId: "1" }}
+              render={({ props }) => {
+                return props && props.city && <CitySummary city={props.city} />;
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
 };
