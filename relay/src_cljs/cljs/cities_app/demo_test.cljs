bafc4d71a06ec03f8a7296b164b254f86240e863
Ky Ky <kyky@kyky.ky>
2020-05-19 19:14:36 +0300

    *

--- relay/src_cljs/cljs/cities_app/demo_test.cljs
+++ relay/src_cljs/cljs/cities_app/demo_test.cljs
@@ -1,5 +1,11 @@
 (ns cljs.cities-app.demo-test
-  (:require [cljs.test :refer (deftest is)]))
+  (:require [cljs.test :refer (deftest is)]
+            ["/cities_app/helpers/uuid" :refer (uuidGen)]))
 
 (deftest a-ok-test
   (is (= 1 1)))
+
+(deftest a-failing-test
+  (do 
+    (js/console.log ((uuidGen "boss")))
+    (is (= 1 2))))
