834ca089caf71872e2367b8c8c639690b2caaf08
Ky Ky <kyky@kyky.ky>
2020-05-20 15:52:46 +0300

    *

--- relay/src_cljs/cljs/cities_app/demo_test.cljs
+++ relay/src_cljs/cljs/cities_app/demo_test.cljs
@@ -1,11 +1,24 @@
 (ns cljs.cities-app.demo-test
-  (:require [cljs.test :refer (deftest is)]
-            ["/cities_app/helpers/uuid" :refer (uuidGen)]))
+  (:require [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer-macros [defspec]]
+            [clojure.test.check.generators :as gen]
+            ; ["/cities_app/helpers/uuid" :refer (uuidGen)]
+            [clojure.test.check.properties :as prop :include-macros true]))
 
-(deftest a-ok-test
-  (is (= 1 1)))
+; (deftest a-ok-test
+;   (is (= 1 1)))
 
-(deftest a-failing-test
-  (do 
-    (js/console.log ((uuidGen "boss")))
-    (is (= 1 2))))
+; (deftest a-failing-test
+;   (do 
+;     (js/console.log ((uuidGen "boss")))
+;     (is (= 1 2))))
+
+; (def sort-idempotent-prop
+;   (prop/for-all [v (gen/vector gen/int)]
+;                 (= (sort v) (sort (sort v)))))
+
+(defspec sort-is-idempotent 100
+  (prop/for-all [v (gen/vector gen/int)]
+                (= (sort v) (sort (sort v)))))
+
+; (println (tc/quick-check 100 sort-idempotent-prop))
