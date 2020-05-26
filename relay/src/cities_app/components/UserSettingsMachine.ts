4c24e8af33f450249d48b317ff183e412c860602
Ky Ky <kyky@kyky.ky>
2020-05-26 13:53:32 +0300

    *

--- relay/src/cities_app/components/UserSettingsMachine.ts
+++ relay/src/cities_app/components/UserSettingsMachine.ts
@@ -1,16 +1,16 @@
 /**
  * {
  *    srv: server state,
- *    muta: active mutation delta,
- *    mutq: queued mutation delta,
+ *    mut: active mutation delta,
+ *    mut2: queued mutation delta,
  *    loc: local draft delta
  * }
  */
 
-function abc(state, event) {
-  if (state.muta === null) {
+function reducer(state, event) {
+  if (state.mut === null) {
     return noMutReducer(state, event);
-  } else if (state.mutq === null) {
+  } else if (state.mut2 === null) {
     return mutReducer(state, event);
   } else {
     return queMutReducer(state, event);
@@ -22,21 +22,52 @@ function noMutReducer(state, event) {
     case "edit": {
       let { fieldName, value } = event.payload;
       if (value !== state.srv[fieldName]) {
+        return { ...state, loc: { ...state.loc, fieldName: value } };
+      }
+      return state;
+    }
+    case "submit": {
+      if (state.loc) {
+        return [{ ...state, mut: state.loc, loc: null }, "startMutation"];
+      }
+      return state;
+    }
+    case "cancel":
+      return { ...state, loc: null };
+    default:
+      return state;
+  }
+}
+
+function mutReducer(state, event) {
+  const optimistic = { ...state.srv, ...state.mut };
+  switch (event.type) {
+    case "edit": {
+      let { fieldName, value } = event.payload;
+      if (value !== optimistic[fieldName]) {
         state = { ...state, loc: { ...state.loc, fieldName: value } };
       }
       return state;
     }
-    case "submit":
-      // code block
-      break;
+    case "submit": {
+      if (state.loc) {
+        return { ...state, mut2: state.loc, loc: null };
+      }
+      return state;
+    }
     case "cancel":
-      // code block
-      break;
+      return { ...state, loc: null };
+    case "mutSucc": {
+      let serverResponse = event.payload;
+      if (state.loc) {
+        let diff = state.loc;
+        return;
+      }
+      return { srv: serverResponse, mut: null, mut2: null, loc: null };
+    }
     default:
-    // code block
+      return state;
   }
 }
 
-function mutReducer(state, event) {}
-
-function queMutReducer(state, event) {}
+function mut2Reducer(state, event) {}
