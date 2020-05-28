65d26f304adf78ae6f833c8bc8902a67f5308017
Ky Ky <kyky@kyky.ky>
2020-05-28 18:40:41 +0300

    *

--- relay/src/cities_app/components/UserSettingsMachine2.ts
+++ relay/src/cities_app/components/UserSettingsMachine2.ts
@@ -1,22 +1,25 @@
 import { UserSettings_user } from "__relay__/UserSettings_user.graphql";
 import { RequireAtLeastOne } from "../helpers/typeUtils";
+import { User } from "../types.codegen";
 
 type UserSettingsType = UserSettings_user["settings"];
 
 type MutZero = {
   status: "idle";
+  srv: UserSettingsType;
 };
 type MutOne = {
   status: "mut";
+  srv: UserSettingsType;
   mut: Partial<UserSettingsType>; // active mutation delta
 };
 type MutTwo = {
   status: "mut2";
+  srv: UserSettingsType;
   mut: Partial<UserSettingsType>;
   mut2: Partial<UserSettingsType>; // queued mutation delta
 };
 
-type MutState = MutZero | MutOne | MutTwo;
 type Edited = Partial<UserSettingsType>;
 
 type StateZeroClean = [MutZero, null];
@@ -35,28 +38,58 @@ type State =
   | StateTwoDirty;
 
 type Event =
-  | { type: "edit"; fieldName: keyof UserSettingsType; value: any }
-  | { type: "submit" }
-  | { type: "cancel" }
-  | { type: "mutSucc"; response: UserSettingsType }
-  | { type: "mutFail" };
+  | EventEdit
+  | EventSubmit
+  | EventCancel
+  | EventMutSucc
+  | EventMutFail;
 
 type EventEdit = {
   type: "edit";
   payload: Partial<UserSettingsType>;
 };
 type EventSubmit = { type: "submit" };
+type EventCancel = { type: "cancel" };
+type EventMutSucc = { type: "mutSucc"; response: UserSettingsType };
+type EventMutFail = { type: "mutFail" };
 
-type X = (
+function transit(state: State, event: Event): State {
+  let [mut, edited] = state;
+  switch (mut.status) {
+    case "idle": {
+      if (edited === null) {
+        return transitZeroCleanEdit(mut, event);
+      }
+      return transitZeroDirtyEdit(mut, edited, event);
+    }
+    case "mut": {
+      if (edited === null) {
+        return transitZeroCleanEdit(mut, event);
+      }
+      return transitZeroDirtyEdit(mut, edited, event);
+    }
+    case "mut2": {
+      if (edited === null) {
+        return transitZeroCleanEdit(mut, event);
+      }
+      return transitZeroDirtyEdit(mut, edited, event);
+    }
+    default:
+      // impossible
+      throw new Error("impossible state is in fact possible");
+  }
+}
+
+function transitZeroCleanEdit(
+  mut: MutZero,
+  event: EventEdit
+): StateZeroClean | StateZeroDirty {
+  return [mut, null];
+}
+
+function transitZeroDirtyEdit(
   state: StateZeroClean,
   event: EventEdit
-) => StateZeroClean | StateZeroDirty;
-
-function reduce(server: UserSettingsType, state: State, event: Event) {
-  const [mut, edited] = state;
-  if (mut.status === "idle" && edited === null) {
-    if (event.type === 'edit') {
-      return 
-    }
-  }
+): StateZeroClean | StateZeroDirty {
+  return state;
 }
