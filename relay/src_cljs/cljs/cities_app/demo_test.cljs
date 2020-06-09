550de09b2cfc06946f7c0eb672026bad5d5f435e
Ky Ky <kyky@kyky.ky>
2020-06-09 19:37:17 +0300

    *

--- relay/src_cljs/cljs/cities_app/demo_test.cljs
+++ relay/src_cljs/cljs/cities_app/demo_test.cljs
@@ -17,22 +17,35 @@
 ;   (prop/for-all [v (gen/vector gen/int)]
 ;                 (= (sort v) (sort (sort v)))))
 
-(defspec sort-is-idempotent 100
-  (prop/for-all [v (gen/vector gen/int)]
-                (= (sort v) (sort (sort v)))))
+;; (defspec sort-is-idempotent 100
+;;   (prop/for-all [v (gen/vector gen/int)]
+;;                 (= (sort v) (sort (sort v)))))
 
-(gen/sample (gen/choose 2 15))
+;; (gen/sample (gen/choose 2 15))
+
+(def user-settings-fields-gen-map
+  {"citiesPaginationPageSize" (gen/choose 2 15)
+   "foo" gen/string
+   "bar" gen/nat})
 
 (def user-settings-gen
- (gen/hash-map
-  "citiesPaginationPageSize" (gen/choose 2 15)
-  "foo" gen/string
-  "bar" gen/nat))
+  (apply gen/hash-map
+         (apply concat user-settings-fields-gen-map)))
 
-(def user-settings-delta-gen 
-  (gen/let [m user-settings-gen
-            k (gen/return (keys m))]
-    (select-keys m k)))
+(def user-settings-delta-gen
+  (gen/bind
+   (apply gen/tuple
+          (repeat (count user-settings-fields-gen-map)
+                  gen/boolean))
+   (fn [bool-seq]
+     (->> (map list
+               (keys user-settings-fields-gen-map)
+               bool-seq)
+          (filter second)
+          (map first)
+          (select-keys user-settings-fields-gen-map)
+          (apply concat)
+          (apply gen/hash-map)))))
 
 (gen/sample user-settings-delta-gen)
 
