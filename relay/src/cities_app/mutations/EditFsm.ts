6b73031711b7c7585d880d99f9b2db3547493eca
Ky Ky <kyky@kyky.ky>
2020-06-17 20:21:37 +0300

    *

--- relay/src/cities_app/mutations/EditFsm.ts
+++ relay/src/cities_app/mutations/EditFsm.ts
@@ -38,27 +38,65 @@ function transit(fsmState, event, context, db) {
 }
 
 function transitFromIdle(event, db) {
-  if (event.type === 'edit') {
-    return [
-      "idle",
-      {},
-      [{ "db/ed": diff(merge(db.ed, event.payload), db.sv) }]
-    ]
-  } else if (event.type === "start-mut") {
-    if (diff(db.ed, db.sv)) {
-      const optUpd = merge(db.sv, db.ed)
+  switch (event.type) {
+    case "clear": {
       return [
-        "mut-pending",
-        { "opt-upd": optUpd },
-        [{ "db/ed": null }, { "commitMutation": optUpd }]
+        "idle",
+        {},
+        [{ "db/ed": null }]
       ]
     }
-    return null
-  } else {
-    return null
+    case "edit": {
+      return [
+        "idle",
+        {},
+        [{ "db/ed": diff(merge(db.ed, event.payload), db.sv) }]
+      ]
+    }
+    case "start-mut": {
+      if (diff(db.ed, db.sv)) {
+        const optUpd = merge(db.sv, db.ed)
+        return [
+          "mut-pending",
+          { "opt-upd": optUpd },
+          [{ "db/ed": null }, { "commitMutation": optUpd }]
+        ]
+      }
+      return null
+    }
+    default:
+      return null
   }
 }
 
 function transiFromMutPending(event, context, db) {
-
+  switch (event.type) {
+    case "clear": {
+      return [
+        "mut-pending",
+        context,
+        [{ "db/ed": null }]
+      ]
+    }
+    case "edit": {
+      return [
+        "mut-pending",
+        context,
+        [{ "db/ed": diff(merge(db.ed, event.payload), db.sv) }]
+      ]
+    }
+    case "start-mut": {
+      if (diff(db.ed, db.sv)) {
+        const optUpd2 = merge(db.sv, db.ed)
+        return [
+          "mut2-queued",
+          { ...context, "opt-upd2": optUpd2 },
+          [{ "db/ed": null }, { "applyUpdate": optUpd2 }]
+        ]
+      }
+      return null
+    }
+    default:
+      return null
+  }
 }
