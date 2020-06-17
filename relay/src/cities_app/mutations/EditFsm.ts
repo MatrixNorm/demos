13b5cff2ba7542af0e259be897baab6536c901c9
Ky Ky <kyky@kyky.ky>
2020-06-17 20:11:46 +0300

    *

---
+++ relay/src/cities_app/mutations/EditFsm.ts
@@ -0,0 +1,64 @@
+
+function merge(obX: object, obY: object) {
+  return { ...obX, ...obY }
+}
+
+function diff(possibleDiff: object, base: object) {
+  if (possibleDiff === null) {
+    return null;
+  }
+  const differentEntries = Object.entries(possibleDiff).filter(
+    //@ts-ignore
+    ([k, v]) => base[k] !== v
+  );
+  if (differentEntries.length > 0) {
+    //@ts-ignore
+    return Object.fromEntries(differentEntries);
+  }
+  return null;
+}
+
+type FsmState = "idle" | "mut-pending" | "mut2-queued"
+
+function transit(fsmState, event, context, db) {
+  switch (fsmState) {
+    case "idle": {
+      return transitFromIdle(event, db);
+    }
+    case "mut-pending": {
+      return transiFromMutPending(event, context, db);
+    }
+    case "mut2-queued": {
+      return //transitFromMut2Queued(state, event, editDelta);
+    }
+    default:
+      // impossible
+      throw new Error("impossible state is in fact possible");
+  }
+}
+
+function transitFromIdle(event, db) {
+  if (event.type === 'edit') {
+    return [
+      "idle",
+      {},
+      [{ "db/ed": diff(merge(db.ed, event.payload), db.sv) }]
+    ]
+  } else if (event.type === "start-mut") {
+    if (diff(db.ed, db.sv)) {
+      const optUpd = merge(db.sv, db.ed)
+      return [
+        "mut-pending",
+        { "opt-upd": optUpd },
+        [{ "db/ed": null }, { "commitMutation": optUpd }]
+      ]
+    }
+    return null
+  } else {
+    return null
+  }
+}
+
+function transiFromMutPending(event, context, db) {
+
+}
