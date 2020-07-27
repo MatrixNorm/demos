63b03160833e5a96513816fd9888552c480313b4
Ky Ky <kyky@kyky.ky>
2020-07-27 23:15:52 +0300

    *

--- relay/src/cities_app/helpers/object.ts
+++ relay/src/cities_app/helpers/object.ts
@@ -1,3 +1,7 @@
+/**
+ * opaque type to guarantee there is no `undefined` values in an object
+ */
+
 declare const __compacted__: unique symbol;
 export type Compacted<T extends object> = Partial<T> & {
   [__compacted__]: true;
@@ -5,8 +9,9 @@ export type Compacted<T extends object> = Partial<T> & {
 
 /**
  * value of `undefined` is treated as absent key
- * value of `null` is interpeted by comsuming application,
- * for this function `null` is a valid value
+ *
+ * how to treat value of `null` is upto consuming application,
+ *
  */
 export function compactObject<T extends object>(
   obj: T | Partial<T> | null | undefined
@@ -20,34 +25,40 @@ export function compactObject<T extends object>(
   return compacted as Compacted<T>;
 }
 
-export function isTrueDelta(delta: any, base: any): boolean {
-  if (!delta) return false;
-  for (let key in delta) {
-    // @ts-ignore
-    if (delta[key] && base[key] !== delta[key]) return true;
-  }
-  return false;
-}
-
 export function trueDelta<T extends object>({
   delta,
   basis,
 }: {
-  delta: Partial<T> | null;
+  delta: Compacted<T> | null;
   basis: T;
-}): Partial<T> | null {
+}): Compacted<T> | null {
   if (!delta) {
     return null;
   }
   const differentEntries = Object.entries(delta).filter(
     //@ts-ignore
-    ([k, v]) => v && basis[k] !== v
+    ([k, v]) => basis[k] !== v
   );
   if (differentEntries.length > 0) {
     //@ts-ignore
     return Object.fromEntries(differentEntries);
   }
   return null;
+}
+
+export function isTrueDelta<T extends object>({
+  delta,
+  basis,
+}: {
+  delta: Compacted<T> | null;
+  basis: T;
+}): boolean {
+  if (!delta) return false;
+  for (let key in delta) {
+    // @ts-ignore
+    if (basis[key] !== delta[key]) return true;
+  }
+  return false;
 }
 
 export function toQueryURL<T extends object>(obj: T) {
--- relay/src/cities_app/mutations/EditControllerReducer.ts
+++ relay/src/cities_app/mutations/EditControllerReducer.ts
@@ -1,14 +1,25 @@
 import { trueDelta, Compacted } from "../helpers/object";
 
-type State<T extends object> =
-  | {
-      status: "idle";
-      value: T;
-      rollback: null;
-      editDelta: Compacted<T>;
-    }
-  | { status: "inFlight"; value: T; rollback: T; editDelta: Compacted<T> }
-  | { status: "hasQueued"; value: T; rollback: T; editDelta: Compacted<T> };
+type State<T extends object> = StateIdle<T> | StateInFlight<T> | StateHasQueued<T>;
+
+type StateIdle<T extends object> = {
+  status: "idle";
+  value: T;
+  rollback: null;
+  editDelta: Compacted<T> | null;
+};
+type StateInFlight<T extends object> = {
+  status: "inFlight";
+  value: T;
+  rollback: T;
+  editDelta: Compacted<T> | null;
+};
+type StateHasQueued<T extends object> = {
+  status: "hasQueued";
+  value: T;
+  rollback: T;
+  editDelta: Compacted<T> | null;
+};
 
 export type Event<T extends object> =
   | EvEdit<T>
@@ -26,6 +37,64 @@ type EvSubmitMutation = { type: "submitMutation" };
 type EvDoneOkMutation = { type: "doneOkMutation" };
 type EvDoneErrMutation = { type: "doneErr<utation" };
 
-export type ReturnType<T extends object> =
+export type ReducerReturn<T extends object> =
   | State<T>
-  | [State<T>, { type: "commitMutation"; mutInput: Compacted<T> }];
+  | { state: State<T>; effects: Effect<T>[] };
+
+type Effect<T extends object> =
+  | { type: "commitMutation"; mutInput: Compacted<T> }
+  | { type: "pushNotification"; message: String };
+
+function reduce<T extends object>(state: State<T>, event: Event<T>) {
+  switch (state.status) {
+    case "idle": {
+      return;
+    }
+    case "inFlight": {
+      return;
+    }
+    case "hasQueued": {
+      return;
+    }
+  }
+}
+
+function reduceIdle<T extends object>(
+  state: StateIdle<T>,
+  event: Event<T>
+): ReducerReturn<T> | null {
+  switch (event.type) {
+    case "edit": {
+      return {
+        ...state,
+        editDelta: trueDelta({
+          delta: { ...state.editDelta, ...event.payload },
+          basis: state.value,
+        }),
+      };
+    }
+    case "submitMutation": {
+      const mutInput = trueDelta({ delta: state.editDelta, basis: state.value });
+      const optimisticValue = { ...state.value, ...state.editDelta };
+      if (mutInput) {
+        return {
+          state: {
+            status: "inFlight",
+            value: optimisticValue,
+            rollback: state.value,
+            editDelta: null,
+          },
+          effects: [{ type: "commitMutation", mutInput }],
+        };
+      }
+      return null;
+    }
+    default:
+      return null;
+  }
+}
+
+function reduceInFlight<T extends object>(
+  state: StateIdle<T>,
+  event: Event<T>
+): ReducerReturn<T> | null {}
