43358ac120529ebb8e543dd913d0b49a6e7d86af
Ky Ky <kyky@kyky.ky>
2020-09-29 16:58:44 +0300

    *

--- relay/src/cities_app/mutations/SearchParametersController.ts
+++ relay/src/cities_app/mutations/SearchParametersController.ts
@@ -8,26 +8,106 @@ import {
 } from "relay-runtime";
 import { retainRecord } from "../helpers/relayStore";
 import { SearchParameters_searchParams } from "__relay__/SearchParameters_searchParams.graphql";
-import { compact, Compacted, merge } from "../helpers/object";
+import { compact, Compacted, merge, shallowEqual } from "../helpers/object";
 import { NukeFragRef, NukeNulls, Writeable } from "../helpers/typeUtils";
 import { SearchParametersControllerQueryResponse } from "__relay__/SearchParametersControllerQuery.graphql";
 
-export type SearchParameters = Compacted<NukeFragRef<SearchParameters_searchParams>>;
+export type SearchParameters = Partial<NukeFragRef<SearchParameters_searchParams>>;
 
-type Event = EditEvent | EnterRouteEvent;
-type EditEvent = { type: "edit"; payload: SearchParameters };
+type SearchParametersPurified = {
+  purified: true;
+  data: SearchParameters;
+};
+
+type Event = EditEvent | EnterRouteEvent | SubmitEvent | CancelEvent;
+type EditEvent = { type: "edit"; delta: SearchParameters };
 type SubmitEvent = { type: "submit" };
 type CancelEvent = { type: "cancel" };
 type EnterRouteEvent = { type: "routeEnter"; urlSearchString: string };
 
+type EventPurified = {
+  rawEvent: Event;
+  payload: SearchParametersPurified;
+};
+
 type State = {
-  searchParams: SearchParameters;
-  editDelta: SearchParameters;
+  searchParams: SearchParametersPurified;
+  editDelta: SearchParametersPurified;
 };
 
 type Effect =
   | { type: "writeSearchParams"; value: SearchParameters }
   | { type: "writeEditDelta"; value: SearchParameters };
+
+function reduce(state: State, event: EventPurified): Effect[] {
+  let eventType = event.rawEvent.type;
+  let eventPayload = event.payload.data;
+  let searchParams = state.searchParams.data;
+  let editDelta = state.editDelta.data;
+
+  switch (eventType) {
+    case "edit": {
+      let _editDelta = { ...editDelta, ...eventPayload };
+      if (shallowEqual(_editDelta, editDelta)) {
+        return [];
+      }
+      return [{ type: "writeEditDelta", value: editDelta }];
+    }
+    case "routeEnter": {
+      return [
+        { type: "writeSearchParams", value: eventPayload },
+        { type: "writeEditDelta", value: {} },
+      ];
+    }
+    case "submit": {
+      return [
+        { type: "writeSearchParams", value: { ...searchParams, ...editDelta } },
+        { type: "writeEditDelta", value: {} },
+      ];
+    }
+    case "cancel": {
+      return [{ type: "writeEditDelta", value: {} }];
+    }
+    default:
+      return [];
+  }
+}
+
+// XXX
+export function purify(
+  input: SearchParameters | null | undefined
+): SearchParametersPurified {
+  if (!input) {
+    return { purified: true, data: {} };
+  }
+  let copy = { ...input };
+  let { countryNameContains } = copy;
+  if (countryNameContains && countryNameContains.trim().length === 0) {
+    delete copy["countryNameContains"];
+  }
+  let denulled = Object.fromEntries(
+    Object.entries(copy).filter(([_, v]) => v !== undefined && v !== null)
+  );
+  return { purified: true, data: denulled };
+}
+
+function purifyEvent(event: Event): EventPurified {
+  let payload: SearchParametersPurified = { purified: true, data: {} };
+  switch (event.type) {
+    case "edit": {
+      payload = purify(event.delta);
+      break;
+    }
+    case "routeEnter": {
+      payload = extractFromUrl(event.urlSearchString);
+      break;
+    }
+  }
+  return {
+    rawEvent: event,
+    payload,
+  };
+}
 
 const QUERY = graphql`
   query SearchParametersControllerQuery {
@@ -50,32 +130,14 @@ function lookupState(environment: IEnvironment): State {
   const response = environment.lookup(operation.fragment);
   const data = response.data as SearchParametersControllerQueryResponse;
   return {
-    searchParams: compact(data?.uiState?.citySearchParams),
-    editDelta: compact(data?.uiState?.citySearchParamsEditDelta),
+    searchParams: purify(data?.uiState?.citySearchParams),
+    editDelta: purify(data?.uiState?.citySearchParamsEditDelta),
   };
-}
-
-function reduce(state: State, event: Event): Effect[] {
-  switch (event.type) {
-    case "edit": {
-      let editDelta = merge(state.editDelta, event.payload);
-      return [{ type: "writeEditDelta", value: editDelta }];
-    }
-    case "routeEnter": {
-      let searchParams = extractSearchParametersFromUrl(event.urlSearchString);
-      return [
-        { type: "writeSearchParams", value: searchParams },
-        { type: "writeEditDelta", value: compact(null) },
-      ];
-    }
-    default:
-      return [];
-  }
 }
 
 export function handleEvent(event: Event, environment: IEnvironment) {
   const state = lookupState(environment);
-  const effects = reduce(state, event);
+  const effects = reduce(state, purifyEvent(event));
   effects.forEach((eff) => {
     if (eff.type === "writeSearchParams") {
       writeSearchParams(eff.value, environment);
@@ -129,7 +191,7 @@ function writeSearchParams(searchParams: SearchParameters, environment: IEnviron
 /**
  * XXX io-ts ???
  */
-function extractSearchParametersFromUrl(urlSearchString: string): SearchParameters {
+function extractFromUrl(urlSearchString: string): SearchParametersPurified {
   let qp = new URLSearchParams(urlSearchString);
   let searchParams = {} as Writeable<SearchParameters>;
   if (qp.has("countryNameContains")) {
